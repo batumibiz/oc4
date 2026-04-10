aeba5d426d390eb785820f4abc4b92072820becf
Oleg Kasyanov <oleg@batumi.org>
2026-04-10 16:53:16 +0400

    Bump minimum PHP version to 8.2

--- www/system/startup.php
+++ www/system/startup.php
@@ -3,8 +3,8 @@
 error_reporting(E_ALL);
 
 // Check Version
-if (version_compare(PHP_VERSION, '8.1', '<')) {
-	exit('PHP8.1+ Required');
+if (version_compare(PHP_VERSION, '8.2', '<')) {
+	exit('PHP8.2+ Required');
 }
 
 if (!ini_get('date.timezone')) {
